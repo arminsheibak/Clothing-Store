9333f2c5960e8325f47a525f2ab9a29eb0d00e93
Armin <armin2003sheibak@gmail.com>
2024-08-09 12:22:06 +0330

    added product to admin

--- back-end/core/admin.py
+++ back-end/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'category']
+    list_editable = ['category', 'price']
+    list_per_page = 10
--- back-end/core/models.py
+++ back-end/core/models.py
@@ -13,3 +13,6 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images')
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
