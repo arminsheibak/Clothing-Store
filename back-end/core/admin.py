bacba5d8dd7ac7a58e48187e3ea54bec308ecaa4
Armin <armin2003sheibak@gmail.com>
2024-08-10 21:06:32 +0330

    register orders in admin

--- back-end/core/admin.py
+++ back-end/core/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order, OrderItem, ShippingInfo
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'category']
     list_editable = ['category', 'price']
     list_per_page = 10
+
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+class ShippingInfoInline(admin.StackedInline):
+    model = ShippingInfo
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "placed_at"]
+    inlines = [OrderItemInline, ShippingInfoInline]
